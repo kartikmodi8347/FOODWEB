721b7201836c4dec2ee5ce2aabea4f1f9a9a1bbc
kartikmodi8347 <kartikmodi8347@gmail.com>
2023-03-28 11:45:28 +0530

    updated

--- public_html/assets/js/foodhut.js
+++ public_html/assets/js/foodhut.js
@@ -45,18 +45,10 @@ function initMap() {
       title: 'Patan, Gujarat'
     });
  }
+     console.log("hello")
 
  
-//  var patan = {lat: 20.5937, lng: 78.9629}; 37.227837  ,  -95.700513
-// var map = new google.maps.Map(document.getElementById('map'), {
-//   zoom: 4,
-//   center: patan
-// });
-// var marker = new google.maps.Marker({
-//   position: patan,
-//   map: map,
-//   title: 'patan'
-// });
+
 
 
 //PATAN,GUJARAT
